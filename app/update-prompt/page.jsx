fd86939f8e26790418274c88565240f3a0fe66a9
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2024-09-25 17:52:49 +0530

    upto promptopia

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Form from "@components/Form";
+
+function page() {
+
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+  const router = useRouter();
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  useEffect(() => {
+    const fetchPrompt = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+    if (promptId) fetchPrompt();
+  }, [promptId]);
+
+  const editPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    if (!promptId) alert("Prompt ID not found");
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/profile");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={editPrompt}
+    />
+  );
+}
+
+export default page;
