82a9234a83155c51c4f6a899b6b7e63f3010a661
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2024-09-28 12:50:44 +0530

    testing

--- app/api/feature-image/route.js
+++ app/api/feature-image/route.js
@@ -6,7 +6,13 @@ export const GET = async () => {
     await connectToDB();
     const images = await FeatureImage.find({});
     if (!images) return new Response("No images found", { status: 404 });
-    return new Response(JSON.stringify(images), { status: 200 });
+    return new Response(JSON.stringify(images), {
+      status: 200,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+      },
+    });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
